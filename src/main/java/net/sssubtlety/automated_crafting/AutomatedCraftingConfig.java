32f51163e5c1e6a859c5e40b1222e0389c79ccc4
BuildTools <unconfigured@null.spigotmc.org>
2020-07-20 21:42:51 -0700

    Versrion 1.0.3. Added config tooltips that remind you to restart. Added stripped stem blocks to stripped_logs.json so they can be used to craft the aut crafter.

--- src/main/java/net/sssubtlety/automated_crafting/AutomatedCraftingConfig.java
+++ src/main/java/net/sssubtlety/automated_crafting/AutomatedCraftingConfig.java
@@ -2,10 +2,14 @@ package net.sssubtlety.automated_crafting;
 
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 
 @Config(name = "automated_crafting")
 public class AutomatedCraftingConfig implements ConfigData {
+    @ConfigEntry.Gui.Tooltip()
     boolean simpleMode = true;
+
+    @ConfigEntry.Gui.Tooltip()
     boolean quasiConnectivity = false;
 
     public boolean isSimpleMode() {
