18a0c222a1742885f9de4bf338c925fa58d9615c
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2021-11-24 21:44:17 -0800

    half-implemented FixedIntSet with raw arrays, realized that won't work (well)

--- src/main/java/net/sssubtlety/automated_crafting/block/connectivity/Connectivity.java
+++
@@ -1,8 +0,0 @@
-package net.sssubtlety.automated_crafting.block.connectivity;
-
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-public interface Connectivity {
-    boolean isPowered(World world, BlockPos pos);
-}
